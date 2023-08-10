86324e26649e8868beb72e87434396c0e922bdd7
Omar-Arja <omaralarja17@gmail.com>
2023-08-10 21:09:33 +0300

    Client <> created search result component

--- instagram-clone-client/src/App.jsx
+++ instagram-clone-client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React, { useState } from 'react';
 import SignUp from './pages/Authentication/Register';
 import SignIn from './pages/Authentication/Log-in';
-import PostCard from './components/post-card';
 import PostFeed from './components/post-feed';
 
 function App() {
---
+++ instagram-clone-client/src/components/search-results.jsx
@@ -0,0 +1,34 @@
+
+
+const SearchResults = ({ id, first_name, last_name, username, is_following }) => {
+    const [is_Following, setIs_Following] = useState(is_following);
+
+    const handleFollow = async () => {
+        try {
+            const res = await axios.post(`http://localhost:8000/api/user/follow/${username}`, {}, { headers: {Authorization: localStorage.getItem('headers')} });
+            setIs_Following(true);
+        } catch (err) {
+            console.log('Error:', err);
+        }
+    };
+
+    const handleUnfollow = async () => {
+        try {
+            const res = await axios.post(`http://localhost:8000/api/user/unfollow/${username}`, {}, { headers: {Authorization: localStorage.getItem('headers')} });
+            setIs_Following(false);
+        } catch (err) {
+            console.log('Error:', err);
+        }
+    };
+
+  return (
+    <div className="user-profile">
+        <div className="user-profile-name">{username} <br/>{first_name} {last_name}</div>
+        <div className="follow/unfollow-btn">
+            {is_Following ? <button className="unfollow-btn" onClick={handleUnfollow}>Unfollow</button> : <button className="follow-btn" onClick={handleFollow}>Follow</button>}
+        </div>
+    </div>
+  );
+};
+
+export default SearchResults;
