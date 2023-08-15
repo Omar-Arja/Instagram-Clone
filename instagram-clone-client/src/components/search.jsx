bdb23c118079b7bb8195a7ea2becac95203663ec
Omar-Arja <omaralarja17@gmail.com>
2023-08-15 03:46:33 +0300

    Client <> optimized search feature

--- instagram-clone-client/src/components/search.jsx
+++ instagram-clone-client/src/components/search.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDebounce } from '@uidotdev/usehooks';
 import axios from 'axios';
 import SearchResults from './search-results';
 
@@ -7,18 +8,23 @@ import SearchResults from './search-results';
 const Search = () => {
     const [search, setSearch] = useState("");
     const [searchResults, setSearchResults] = useState([]);
+
+    const debouncedSearch = useDebounce(search, 500);
+
+    useEffect(() => {
+        if (debouncedSearch) {
+            handleSearch();
+        } else {
+            setSearchResults([]);
+        }
+    }, [debouncedSearch]);
     
     const handleSearch = async (e) => {
-        if (search == "") {
-            setSearchResults([]);
-            return;
-        };
 
         try {
             const res = await axios.get(`http://localhost:8000/api/user/search/${search}`, { headers: {Authorization: localStorage.getItem('headers')} });
             setSearchResults(res.data.users);
         } catch (err) {
-            console.log('Error:', err);
         }
     }
 
@@ -27,7 +33,7 @@ const Search = () => {
         <div className="search-container">
             <div className="search-bar">
                 <input type="text" placeholder="Search" onChange={(e) => setSearch(e.target.value)} />
-                <button onClick={handleSearch}>Search</button>  
+                {/* <button onClick={handleSearch}>Search</button>   */}
             </div>
             {searchResults.length > 0 && (
                 <div className="search-results">
